f623cab8c9b20cdd0b5c411603e4aabcb08e846f
Ester de Sousa <estersousa_ata@outlook.com>
2026-08-02 15:06:43 -0300

    Add basic SQL select example

---
+++ queries/select-basico.sql
@@ -0,0 +1,11 @@
+-- ==========================================
+-- SELECT Básico
+-- Exemplo para estudos em QA
+-- ==========================================
+
+SELECT
+    COD_CONHECIMENTOS,
+    COD_FILIAIS,
+    DT_EMISSAO,
+    VL_TOTAL
+FROM TB_CONHECIMENTOS;
